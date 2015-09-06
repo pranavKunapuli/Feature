0c888fcbc6dd2924d5feb726eb481bd42e57a9d4
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2015-09-06 04:15:47 -0400

    Designing Front End

--- app/controllers/performer_home.js
+++ app/controllers/performer_home.js
@@ -1,3 +1,7 @@
 angular.module("phiture").controller("PerformerHomeController", function($scope, $location, $routeParams, firebase) {
     var ref = firebase;
-})
+
+    $scope.settings = function() {
+        $location.path("/performer_homepage/:id/settings");
+    };
+});
